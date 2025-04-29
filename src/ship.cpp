09f345d56dac1b2ddead6b6ab1629d69bba42c58
moleculeATP <lou-alepeneloux@outlook.com>
2025-04-29 12:19:50 +0200

    arret de la compilation, ca ne marche plus hihihi

--- src/ship.cpp
+++ src/ship.cpp
@@ -11,6 +11,10 @@ ship::ship(const mesh_drawable& body_init)
     velocity = { 0,0,0 };
     rotation = { 0,0,0 };
     speed = 0.1f;
+
+    inputs = nullptr;
+    window = nullptr;
+    is_cursor_trapped = false;
 }
 
 ship::ship()
@@ -21,9 +25,30 @@ ship::ship()
     velocity = { 0,0,0 };
     rotation = { 0,0,0 };
     speed = 0.1f;
+
+    inputs = nullptr;
+    window = nullptr;
+    is_cursor_trapped = false;
 }
 
 void ship::draw(environment_generic_structure const& environment){
     cgp::draw(body, environment);
 }
+
+void ship::idle_frame(mat4& camera_matrix_view)
+{
+    // Update the ship's position and rotation based on its speed and velocity
+    // Preconditions
+	assert_cgp_no_msg(inputs != nullptr);
+	assert_cgp_no_msg(window != nullptr);
+
+	float const magnitude = inputs->time_interval;
+
+
+
+
+    position += velocity * speed;
+    body.model.translation = position;
+
+
 }
--- src/ship.hpp
+++ src/ship.hpp
@@ -10,10 +10,21 @@ struct ship {
     vec3 rotation;
     float speed;
 
+    // Pointers to the global state of the inputs (keyboard, mouse, etc)
+    input_devices* inputs;
+    // Pointer to the global state of the window
+    window_structure* window;
+
+
     // Constructeur
     ship(const mesh_drawable& body_init);
     ship();
 
     void draw(environment_generic_structure const& environment);
+
+    void idle_frame(mat4& camera_matrix_view);
+
+protected:
+    bool is_cursor_trapped; // true = cursor captured (/infinite motion), false = cursor free
 };
 }
